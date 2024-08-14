0bb3d476373968d0e3933f57562b1618125e6864
digitalneedstech <69498018+digitalneedstech@users.noreply.github.com>
2024-08-14 23:36:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,28 +16,26 @@ app.add_middleware(
 
 
 @app.get("/")
-async def root(requestToken):
+def root(requestToken):
     print(requestToken)
     url = 'https://developer.paytmmoney.com/accounts/v2/gettoken'
     headers = {
         "Content-Type": "application/json"
     }
-    response_json = ""
     try:
         # Make a GET request to the API endpoint using requests.get()
-        response = requests.post(url, headers=headers, data={
+        response = requests.post(url, headers=headers,json={
             "api_key": "a254798bb6084fbf95cb1e5512c2781d",
             "api_secret_key": "35919b6920f742b891c69b32e34545c4",
             "request_token": requestToken
         })
-        
         # Check if the request was successful (status code 200)
         if response.status_code == 200:
             response_json = response.json()
-            print(response_json)
-            return response_json
+            return {"token": requestToken, "response": response_json}
         else:
-            print('Error:', response.status_code)
+            print(response.text)
+    except Exception:
+        print("Exception encountered")
     finally:
         print("in finally block")
-    return {"token": requestToken, "response": response_json}
