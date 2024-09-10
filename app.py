c8793216971dca218535551ba178650d7ccc0b6c
digitalneeds <digitalneeds.tech@gmail.com>
2024-09-10 18:01:50 +0530

    Update environment variables

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, request 
 import requests
+from dotenv import dotenv_values
+from flask import Flask, request
 
 app = Flask(__name__)
+config = dotenv_values(".env")
 
 @app.route('/')
 def hello_world():
@@ -11,7 +13,6 @@ def get_hello_world():
     if request.method == "GET":
         print(request.args)
         requestToken = request.args.get("requestToken")
-        print(requestToken)
         url = 'https://developer.paytmmoney.com/accounts/v2/gettoken'
         headers = {
             "Content-Type": "application/json"
@@ -19,8 +20,8 @@ def get_hello_world():
         try:
             # Make a GET request to the API endpoint using requests.get()
             response = requests.post(url, headers=headers, json={
-                "api_key": "xxx",
-                "api_secret_key": "xxx",
+                "api_key": config["API_KEY"],
+                "api_secret_key": config["API_SECRET"],
                 "request_token": requestToken
             })
             # Check if the request was successful (status code 200)
